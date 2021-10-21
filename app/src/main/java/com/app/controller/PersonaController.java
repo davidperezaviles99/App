69a21b0d21dfc3cd907b646d8b11eb52b3603064
davidperezaviles99 <45419830+davidperezaviles99@users.noreply.github.com>
2021-10-21 11:22:58 +0200

    Actuator

--- app/src/main/java/com/app/controller/PersonaController.java
+++ app/src/main/java/com/app/controller/PersonaController.java
@@ -1,6 +1,9 @@
 package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
